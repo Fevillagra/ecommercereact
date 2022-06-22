8d3e0edb13fe940314d9e2b71aa73d84a8cc6d0d
Facundo Ezequiel Villagra <ezefac.95@gmail.com>
2022-06-21 21:40:25 -0300

    Modificaciones generales/Nav movil Footer completado

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,26 +2,55 @@ import './Footer.css'
 
 function Footer() {
     return(
-        <footer className="footer">
+        <footer className="footer" id="f">
             <div className="footer__content">
                 <div className="footer__content--main">
                     <h3>Pepper ©</h3>
                     <p>Software And Hardware.</p>
                 </div>
-                <div className="footer__content--store">
+                <div className="footer__content--divlist">
                     <h3>Store</h3>
                     <ul>
-                        <li><a>Phone</a></li>
-                        <li><a>Pc</a></li>
-                        <li><a>Tv And Home</a></li>
-                        <li><a>Watch</a></li>
-                        <li><a>Accesories</a></li>
-                        <li><a>GiftCards</a></li>
-                        <li><a>Components</a></li>
+                        <li><a href="#f">Phone</a></li>
+                        <li><a href="#f">Pc</a></li>
+                        <li><a href="#f">Tv And Home</a></li>
+                        <li><a href="#f">Watch</a></li>
+                        <li><a href="#f">Accesories</a></li>
+                        <li><a href="#f">GiftCards</a></li>
+                        <li><a href="#f">Components</a></li>
                     </ul>
                 </div>
-                <div>Support</div>
-                <div>Contact</div>
+                <div className="footer__content--divlist">
+                    <h3>Services</h3>
+                    <ul>
+                        <li><a href="#f">Pepper Music</a></li>
+                        <li><a href="#f">Pepper TV+</a></li>
+                        <li><a href="#f">Pepper Books</a></li>
+                        <li><a href="#f">Pepper Arcade</a></li>
+                        <li><a href="#f">Pepper Card</a></li>
+                        <li><a href="#f">Pepper Podcast</a></li>
+                    </ul>
+                </div>
+                
+                <div className="footer__content--divlist">
+                    <h3>Us</h3>
+                    <ul>
+                        <li><a href="#f">About Us</a></li>
+                        <li><a href="#f">Contact Us</a></li>
+                        <li><a href="#f">Affiliates</a></li>
+                        <li><a href="#f">Resources</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div className="footer__media">
+                <div className="footer__media--div"><a href="#f"><i class="fa-brands fa-facebook-f"></i></a></div>
+                <div className="footer__media--div"><a href="#f"><i class="fa-brands fa-twitter"></i></a></div>
+                <div className="footer__media--div"><a href="#f"><i class="fa-brands fa-youtube"></i></a></div>
+                <div className="footer__media--div"><a href="#f"><i class="fa-brands fa-instagram"></i></a></div>
+                <div className="footer__media--div"><a href="#f"><i class="fa-brands fa-linkedin-in"></i></a></div>
+            </div>
+            <div className="footer__copy">
+                <p>©Copyright. All rights reserved.</p>
             </div>
         </footer>
     )
